5d9cfbd412a639188e52cf95a647803d84ea9177
yogesh <yogeshkamat181@gmail.com>
2020-06-19 11:46:52 +0530

    add init.el

---
+++ init.el
@@ -0,0 +1,30 @@
+;;; init.el --- My GNU Emacs Configuration
+
+;; Copyright (C) 2020 Yogesh Kamat
+
+;; Author: Yogesh Kamat <yogeshkamat181@gmail.com>
+;; Keywords: Convenience
+;; URL:
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file contains all the tweaks that I like to use with GNU Emacs.
+
+;;; Code:
+
+;;; init.el ends here
